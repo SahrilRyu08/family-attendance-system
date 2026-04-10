813c278f7c986bc072869851ae37bbc7709cecb4
sahril saepuloh <SahrilRyu08@github.com>
2026-04-10 12:49:22 +0700

    Initial create spring project

---
+++ backend/attendance-service/src/main/kotlin/com/family/attendance_service/AttendanceServiceApplication.kt
@@ -0,0 +1,11 @@
+package com.family.attendance_service
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AttendanceServiceApplication
+
+fun main(args: Array<String>) {
+	runApplication<AttendanceServiceApplication>(*args)
+}
---
+++ backend/attendance-service/src/test/kotlin/com/family/attendance_service/AttendanceServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.family.attendance_service
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AttendanceServiceApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
